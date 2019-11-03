8b4d34180de52b0faaf57dd25d09afc0c5cab3ba
Jason Barnett <jason.w.barnett@gmail.com>
2019-11-03 08:50:10 -0500

    add more

--- lib/bart/station.rb
+++ lib/bart/station.rb
@@ -2,6 +2,28 @@
 
 class Bart
   class Station
+    def api
+      @api ||= Faraday.new(url: "http://api.bart.gov/api") do |faraday|
+        faraday.params[:key] = "MW9S-E7SL-26DU-VV8V"
+        faraday.params[:json] = "y"
+        faraday.response :json
+        if false
+          faraday.response :logger do |logger|
+            logger.filter(/(api_key=)(\w+)/, '\1[REMOVED]')
+          end
+        end
+        faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
+      end
+    end
+
+    def test
+      # http://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=MW9S-E7SL-26DU-VV8V&json=y
+      api.get do |req|
+        req.params["cmd"] = "etd"
+        req.params["orig"] = "RICH"
+      end
+    end
+
     def name; end
 
     def abbreviation; end
